a205526ac5c5c66b78201b56686808d9e6401204
urijan44 <teamsva360@gmail.com>
2022-10-26 01:28:52 +0900

    Initial Commit

---
+++ SwiftUIViewLifeCycle/SwiftUIViewLifeCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewLifeCycleApp.swift
+//  SwiftUIViewLifeCycle
+//
+//  Created by hoseung Lee on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewLifeCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
